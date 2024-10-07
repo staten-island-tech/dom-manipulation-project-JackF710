aac762a61ec79d36ef2fca9f1881b1ef0cf8a841
JackF710 <jtfeery@gmail.com>
2024-10-07 13:39:44 -0400

    hgf

---
+++ notes.js
@@ -0,0 +1,27 @@
+const students = ["Peter", "Christina", "Kim"];
+// console.log(students[0]);
+students.forEach((student) => console.log(student));
+
+const student = [
+  {
+    name: "Christina",
+    age: 15,
+    graduated: false,
+  },
+  {
+    name: "Laith",
+    age: 18,
+    graduated: True,
+  },
+  {
+    name: "Eyed",
+    age: 16,
+    graduated: false,
+  },
+];
+
+//students.forEach((student) => console.log(student.name))
+
+students
+  .filter((student) => student.graduated === false)
+  .forEarch((student) => console.log(student.name));
