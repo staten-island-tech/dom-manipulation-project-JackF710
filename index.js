99b9c46cd8d2e9756b3ab0e15c53a55956829c5a
JackF710 <jtfeery@gmail.com>
2024-10-09 13:56:16 -0400

    afhdf

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(document.querySelector("input").value);
 });
+
+//select all buttons as nodelist (can use for each)
+const btns = document.querySelectorAll("btn");
+//make array from buttons if i want to use filter etc.
+const newbtns = Array.from(btns);
+//iterate through array and change each buttons color
+newbtns.forEach((btn) => (btn.style.backgroundColor = "blue"));
+
+newbtns.forEach((btn) =>
+  btn.addEventListener("click", function (event) {
+    console.log(event.target.textContent);
+  })
+);
+
+DOMSelectors.container.insertAdjacentHTML(
+  "beforeend",
+  '<div class="card"><h2 class="card header">${movie.title}</h2></div>'
+);
--- notes.js
+++ notes.js
@@ -25,3 +25,37 @@ const student = [
 students
   .filter((student) => student.graduated === false)
   .forEach((student) => console.log(student.name));
+
+//select all buttons as nodelist (can use for each)
+const buttons = document.querySelectorAll("button");
+//make array from buttons if i want to use filter etc.
+const newButtons = Array.from(buttons);
+//iterate through array and change each buttons color
+newButtons.forEach((button) => (button.style.backgroundColor = "blue"));
+
+newButtons.forEach((button) =>
+  button.addEventListener("click", function (event) {
+    console.log(event.target.textContent);
+  })
+);
+
+DOMSelectors.container.insertAdjacentHTML(
+  "beforeend",
+  '<div class="card"><h2 class="card header">${movie.title}</h2></div>'
+);
+
+function getRGB() {
+  let R = Math.floor(Math.random() * 256);
+  let G = Math.floor(Math.random() * 256);
+  let B = Math.floor(Math.random() * 256);
+  return { R, G, B }; //ends function
+}
+//create the HTML for inputs, cards and container aka where the cards go
+
+//select/query the HTML form and get values from inputs
+
+//turn values into object Movie, Game, User
+
+//insert card with object into HTML
+
+//add event listener for remove button in JS
