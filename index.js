597984f9c26c9f0631955921658edbf1a20369ce
JackF710 <jtfeery@gmail.com>
2024-10-20 13:37:28 -0400

    good

--- index.js
+++ index.js
@@ -10,22 +10,26 @@ DOMSelectors.form.addEventListener("submit", function (event) {
   const rating = document.getElementById("player-rating").value;
   const imageUrl = document.getElementById("player-image").value;
 
-  const playerCardHTML = `
-    <div class="fifa-card">
-      <div class="fifa-rating">${rating}</div>
-      <div class="fifa-image">
-        <img src="${imageUrl}" alt="Player Image">
-      </div>
-      <div class="fifa-info">
-        <p class="fifa-name">${name}</p>
-      </div>
+  const playerCard = document.createElement("div");
+  playerCard.classList.add("fifa-card");
+
+  playerCard.innerHTML = `
+    <div class="fifa-rating">${rating}</div>
+    <div class="fifa-image">
+      <img src="${imageUrl}" alt="Player Image">
     </div>
+    <div class="fifa-info">
+      <p class="fifa-name">${name}</p>
+    </div>
+    <button class="remove-button">Remove</button>
   `;
 
-  DOMSelectors.playerCardContainer.insertAdjacentHTML(
-    "beforeend",
-    playerCardHTML
-  );
+  DOMSelectors.playerCardContainer.appendChild(playerCard);
 
   DOMSelectors.form.reset();
+
+  const removeButton = playerCard.querySelector(".remove-button");
+  removeButton.addEventListener("click", function () {
+    DOMSelectors.playerCardContainer.removeChild(playerCard);
+  });
 });
